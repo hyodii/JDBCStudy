01b1f29840dd3402bedc6cc8fdafdee3a5b1254b
HyoJin Jeong <87711421+hyodii@users.noreply.github.com>
2021-09-29 12:56:57 +0900

    데이터 저장 및 전송 객체 구성

---
+++ JDBC/JDBC02/MemberDTO.java
@@ -0,0 +1,48 @@
+/*====================
+	MemberDTO.java
+======================*/
+
+package com.test;
+
+public class MemberDTO
+{
+	// 주요 속성 구성
+	private String sid, name, tel;
+
+	// getter / setter 구성
+	public String getSid()
+	{
+		return sid;
+	}
+
+	public void setSid(String sid)
+	{
+		this.sid = sid;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getTel()
+	{
+		return tel;
+	}
+
+	public void setTel(String tel)
+	{
+		this.tel = tel;
+	}
+	
+	// getter / setter 구성 → 마우스 우클릭 → Source → Generate getter and setter
+	
+	
+	
+
+}
